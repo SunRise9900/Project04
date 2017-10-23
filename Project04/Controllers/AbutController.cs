ceebe93475979eaf596b7ab5ffaabdca9e968644
SunRise9900 <kbaomar@hotmail.com>
2017-10-23 18:44:59 +0400

    Add project files.

---
+++ Project04/Controllers/AbutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project04.Controllers
+{
+    public class AbutController : Controller
+    {
+        // GET: Abut
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
